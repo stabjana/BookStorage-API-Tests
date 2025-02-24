edd9f54d761ff7623a163d1c8a3edcd0919a024c
Jana Stefanie <s2400789@BC-CWRW4DVTW1-Kannettava-tietokone.local>
2025-02-24 12:11:25 +0200

    add test cases md and constructor test

--- __test__/bookstorage.test.js
+++ __test__/bookstorage.test.js
@@ -1,3 +1,12 @@
+'use strict';
+
 const BookStorage = require('../bookstorage');
 const defaultData = require('../datastorage.json');
+
+describe('Testing constructor', () => {
+    test('Test 1: missing parameter throw an exception', () => {
+        expect(() => new BookStorage())
+            .toThrow('data storage missing');
+    });
+});
 
